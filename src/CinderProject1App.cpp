6f3f968046c6dcc93cbbab93ce100da12f5232ee
Ben Decker <deckerbd@muohio.edu>
2012-08-23 11:55:12 -0400

    Initial Commit

---
+++ src/CinderProject1App.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CinderProject1App : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void CinderProject1App::setup()
+{
+}
+
+void CinderProject1App::mouseDown( MouseEvent event )
+{
+}
+
+void CinderProject1App::update()
+{
+}
+
+void CinderProject1App::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 255, 0, 0 ) ); 
+}
+
+CINDER_APP_BASIC( CinderProject1App, RendererGl )
