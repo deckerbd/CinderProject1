4a0ccc533c2e35902df8d60393f40439d8c1de2e
deckerbd <deckerbd@muohio.edu>
2012-08-27 11:16:16 -0300

    Background now draws random colors

--- src/CinderProject1App.cpp
+++ src/CinderProject1App.cpp
@@ -1,5 +1,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include <ctime>
+#include <cstdlib>
 
 using namespace ci;
 using namespace ci::app;
@@ -27,8 +29,16 @@ void CinderProject1App::update()
 
 void CinderProject1App::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 1.0, 0, 0 ) ); 
+	int redvalue, greenvalue, bluevalue;
+	srand(time(0));
+	redvalue=(rand() % 255)+1;
+	greenvalue=(rand() % 255)+1;
+	bluevalue=(rand()% 255)+1;
+	// clear out the window and draw random color
+	for(int a=2; a>1; a=a+1)
+	{
+		gl::clear( Color( redvalue / 255.0 , greenvalue / 255.0, bluevalue / 255.0 ) );
+	}
 }
 
 CINDER_APP_BASIC( CinderProject1App, RendererGl )
